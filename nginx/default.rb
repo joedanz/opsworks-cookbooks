219718f57f785f6e8f0fc28864be97744d91a361
Joe Danziger <joe@ticc.net>
2015-05-04 13:53:33 -0400

    update uWSGI install

--- nginx/default.rb
+++ nginx/default.rb
@@ -30,6 +30,6 @@ end
 Chef::Log.info("******Starting WSGI.******")
 
 execute "Install nginx" do
-    command "sudo /home/search/env/bin/uwsgi --socket 127.0.0.1:8080 -w WSGI:app &"
+    command "sudo uwsgi --socket 127.0.0.1:8080 -w WSGI:app &"
     action :run
 end

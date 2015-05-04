67dcd2b57a472a36c6a67fee6830cc07374fe402
Joe Danziger <joe@ticc.net>
2015-05-04 16:23:39 -0400

    add user:group

--- git/recipes/checkout.rb
+++ git/recipes/checkout.rb
@@ -3,5 +3,7 @@ Chef::Log.info("******Git - Checkout Application.******")
 git "/home/search" do
   repository "git://github.com/joedanz/flask-elasticsearch.git"
   revision "master"
+  user "ec2-user"
+  group "ec2-user"
   action :sync
 end

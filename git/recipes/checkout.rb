206cdf8ce4ada7df5493d93365ed7fa7d01ad998
Joe Danziger <joe@ticc.net>
2015-05-04 16:31:09 -0400

    move under home folder

--- git/recipes/checkout.rb
+++ git/recipes/checkout.rb
@@ -1,14 +1,9 @@
 Chef::Log.info("******Git - Checkout Application.******")
 
-git "/home/search" do
+git "/home/ec2-user/search" do
   repository "git://github.com/joedanz/flask-elasticsearch.git"
   revision "master"
+  user "ec2-user"
+  group "ec2-user"
   action :sync
-end
-
-Chef::Log.info("******Git - Update Folder Ownership.******")
-
-execute "Update Permissions" do
-    command "sudo chown -R ec2-user:ec2-user /home/search"
-    action :run
 end

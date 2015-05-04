cf6ac95afd45bc8bd2a64c4323251bf59fc8327d
Joe Danziger <joe@ticc.net>
2015-05-04 16:29:11 -0400

    update checkout recipe

--- git/recipes/checkout.rb
+++ git/recipes/checkout.rb
@@ -3,7 +3,12 @@ Chef::Log.info("******Git - Checkout Application.******")
 git "/home/search" do
   repository "git://github.com/joedanz/flask-elasticsearch.git"
   revision "master"
-  user "ec2-user"
-  group "ec2-user"
   action :sync
+end
+
+Chef::Log.info("******Git - Update Folder Ownership.******")
+
+execute "Update Permissions" do
+    command "sudo chown -R ec2-user:ec2-user /home/search"
+    action :run
 end

03db1c2b192dab424548460eb9aed2fc91995cfc
Joe Danziger <joe@ticc.net>
2015-05-04 22:44:48 -0400

    move gunicorn recipes to default

--- gunicorn/recipes/default.rb
+++ gunicorn/recipes/default.rb
@@ -1,3 +1,10 @@
+Chef::Log.info("******Installing gunicorn.******")
+
+execute "Install gunicorn" do
+    command "/home/ec2-user/search/env/bin/pip install gunicorn"
+    action :run
+end
+
 Chef::Log.info("******Running gunicorn.******")
 
 execute "Run gunicorn" do
--- gunicorn/recipes/install.rb
+++
@@ -1,6 +0,0 @@
-Chef::Log.info("******Installing gunicorn.******")
-
-execute "Install gunicorn" do
-    command "/home/ec2-user/search/env/bin/pip install gunicorn"
-    action :run
-end

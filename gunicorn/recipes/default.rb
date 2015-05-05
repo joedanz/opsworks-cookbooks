ebfece5cdf2f94941d055e4b2cffec8843cffb94
Joe Danziger <joe@ticc.net>
2015-05-04 23:00:56 -0400

    add & to start daemon

--- gunicorn/recipes/default.rb
+++ gunicorn/recipes/default.rb
@@ -8,6 +8,6 @@ end
 Chef::Log.info("******Running gunicorn.******")
 
 execute "Run gunicorn" do
-    command "/home/ec2-user/search/env/bin/gunicorn -b 0.0.0.0:8080 app:app"
+    command "/home/ec2-user/search/env/bin/gunicorn -b 0.0.0.0:8080 app:app &"
     action :run
 end

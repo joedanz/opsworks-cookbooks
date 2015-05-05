77a61d94e798bca61b7d902ac7e1fbd5c9f45007
Joe Danziger <joe@ticc.net>
2015-05-04 23:17:02 -0400

    add cwd

--- gunicorn/recipes/default.rb
+++ gunicorn/recipes/default.rb
@@ -8,6 +8,7 @@ end
 Chef::Log.info("******Running gunicorn.******")
 
 execute "Run gunicorn" do
-    command "/home/ec2-user/search/env/bin/gunicorn -b 0.0.0.0:8080 app:app &"
+    cwd '/home/ec2-user/search'
+    command "env/bin/gunicorn -b 0.0.0.0:8080 app:app &"
     action :run
 end

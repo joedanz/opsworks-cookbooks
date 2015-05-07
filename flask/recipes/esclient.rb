07f92e1c04decab195d6fa30773ffc1f5e47d779
Joe Danziger <joe@ticc.net>
2015-05-06 23:30:27 -0400

    use official elasticsearch client

--- flask/recipes/esclient.rb
+++ flask/recipes/esclient.rb
@@ -1,6 +1,6 @@
 Chef::Log.info("******Installing Flask ElasticSearch Client.******")
 
 execute "Install Flask" do
-    command "/home/ec2-user/search/env/bin/pip install Flask-ESClient"
+    command "/home/ec2-user/search/env/bin/pip install elasticsearch"
     action :run
 end
